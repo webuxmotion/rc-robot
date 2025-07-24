de61ceb1d5e838307d29351f064f514de73fde7e
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-07-24 18:50:19 +0300

    Send throttle and display on lilka display

--- src/main.cpp
+++ src/main.cpp
@@ -3,47 +3,53 @@
 #include <WiFiUdp.h>
 
 // Wi-Fi налаштування
-const char* ssid = "C3-Mini-AP";
-const char* password = "12345678";
-const char* c3MiniIP = "192.168.4.1";
+const char *ssid = "C3-Mini-AP";
+const char *password = "12345678";
+const char *c3MiniIP = "192.168.4.1";
 const int udpPort = 8888;
 
 WiFiUDP udp;
 
 // Дані, які надсилаємо
-struct ControlData {
+struct ControlData
+{
   int throttle;
 };
 
-ControlData controlData = {190};  // Початковий газ
+ControlData controlData = {340}; // Початковий газ
 
 // Межі
-const int minThrottle = 190;
+const int minThrottle = 340;
 const int maxThrottle = 1811;
-const int step = 1;
+const int step = 2;
 
-void setup() {
+void setup()
+{
   lilka::begin();
   Serial.begin(9600);
 
   // Підключення до Wi-Fi
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("\nConnected!");
 }
 
-void loop() {
+void loop()
+{
   lilka::State state = lilka::controller.getState();
 
   // Плавна зміна throttle
-  if (state.up.pressed) {
+  if (state.up.pressed)
+  {
     controlData.throttle += step;
   }
-  if (state.down.pressed) {
+  if (state.down.pressed)
+  {
     controlData.throttle -= step;
   }
 
@@ -53,9 +59,16 @@ void loop() {
   Serial.print("Throttle: ");
   Serial.println(controlData.throttle);
 
+  // Відображення на Lilka дисплеї
+  lilka::display.fillScreen(lilka::colors::Black); // Заповнити екран червоним кольором
+  lilka::display.setCursor(20, 100);
+  lilka::display.setTextColor(lilka::colors::White); // Зелений текст
+  lilka::display.setTextSize(2);
+  lilka::display.print(controlData.throttle);
+
   // Надсилання даних на C3-Mini
   udp.beginPacket(c3MiniIP, udpPort);
-  udp.write((uint8_t*)&controlData, sizeof(controlData));
+  udp.write((uint8_t *)&controlData, sizeof(controlData));
   udp.endPacket();
 
   delay(20);
